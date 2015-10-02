f95ad1ba36452bc8d9ccecd29cb2c0fd4c69a59a
Gabriel Kenworthy <gkpower15@gmail.com>
2015-10-01 23:08:37 -0600

    Finally set up for implementing abstract methods

--- PS4/Class1.cs
+++ PS4/Class1.cs
@@ -3,7 +3,161 @@ using AbstractSpreadsheet;
 
 public class Spreadsheet : AbstractSpreadsheet
 {
-	public Spreadsheet()
-	{
-	}
+    public Spreadsheet()
+    { 
+       
+    }
+    /// <summary>
+    /// Enumerates the names of all the non-empty cells in the spreadsheet.
+    /// </summary>
+    public abstract IEnumerable<String> GetNamesOfAllNonemptyCells();
+
+
+    /// <summary>
+    /// If name is null or invalid, throws an InvalidNameException.
+    /// 
+    /// Otherwise, returns the contents (as opposed to the value) of the named cell.  The return
+    /// value should be either a string, a double, or a Formula.
+    public abstract object GetCellContents(String name);
+
+
+    /// <summary>
+    /// If name is null or invalid, throws an InvalidNameException.
+    /// 
+    /// Otherwise, the contents of the named cell becomes number.  The method returns a
+    /// set consisting of name plus the names of all other cells whose value depends, 
+    /// directly or indirectly, on the named cell.
+    /// 
+    /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+    /// set {A1, B1, C1} is returned.
+    /// </summary>
+    public abstract ISet<String> SetCellContents(String name, double number);
+
+    /// <summary>
+    /// If text is null, throws an ArgumentNullException.
+    /// 
+    /// Otherwise, if name is null or invalid, throws an InvalidNameException.
+    /// 
+    /// Otherwise, the contents of the named cell becomes text.  The method returns a
+    /// set consisting of name plus the names of all other cells whose value depends, 
+    /// directly or indirectly, on the named cell.
+    /// 
+    /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+    /// set {A1, B1, C1} is returned.
+    /// </summary>
+    public abstract ISet<String> SetCellContents(String name, String text);
+
+    /// <summary>
+    /// If the formula parameter is null, throws an ArgumentNullException.
+    /// 
+    /// Otherwise, if name is null or invalid, throws an InvalidNameException.
+    /// 
+    /// Otherwise, if changing the contents of the named cell to be the formula would cause a 
+    /// circular dependency, throws a CircularException.  (No change is made to the spreadsheet.)
+    /// 
+    /// Otherwise, the contents of the named cell becomes formula.  The method returns a
+    /// Set consisting of name plus the names of all other cells whose value depends,
+    /// directly or indirectly, on the named cell.
+    /// 
+    /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+    /// set {A1, B1, C1} is returned.
+    /// </summary>
+    public abstract ISet<String> SetCellContents(String name, Formula formula);
+
+
+    /// <summary>
+    /// If name is null, throws an ArgumentNullException.
+    /// 
+    /// Otherwise, if name isn't a valid cell name, throws an InvalidNameException.
+    /// 
+    /// Otherwise, returns an enumeration, without duplicates, of the names of all cells whose
+    /// values depend directly on the value of the named cell.  In other words, returns
+    /// an enumeration, without duplicates, of the names of all cells that contain
+    /// formulas containing name.
+    /// 
+    /// For example, suppose that
+    /// A1 contains 3
+    /// B1 contains the formula A1 * A1
+    /// C1 contains the formula B1 + A1
+    /// D1 contains the formula B1 - C1
+    /// The direct dependents of A1 are B1 and C1
+    /// </summary>
+    protected abstract IEnumerable<String> GetDirectDependents(String name);
+
+
+    /// <summary>
+    /// Requires that names be non-null.  Also requires that if names contains s,
+    /// then s must be a valid non-null cell name.
+    /// 
+    /// If any of the named cells are involved in a circular dependency,
+    /// throws a CircularException.
+    /// 
+    /// Otherwise, returns an enumeration of the names of all cells whose values must
+    /// be recalculated, assuming that the contents of each cell named in names has changed.
+    /// The names are enumerated in the order in which the calculations should be done.  
+    /// 
+    /// For example, suppose that 
+    /// A1 contains 5
+    /// B1 contains 7
+    /// C1 contains the formula A1 + B1
+    /// D1 contains the formula A1 * C1
+    /// E1 contains 15
+    /// 
+    /// If A1 and B1 have changed, then A1, B1, and C1, and D1 must be recalculated,
+    /// and they must be recalculated in either the order A1,B1,C1,D1 or B1,A1,C1,D1.
+    /// The method will produce one of those enumerations.
+    /// 
+    /// PLEASE NOTE THAT THIS METHOD DEPENDS ON THE ABSTRACT METHOD GetDirectDependents.
+    /// IT WON'T WORK UNTIL GetDirectDependents IS IMPLEMENTED CORRECTLY.
+    /// </summary>
+    protected IEnumerable<String> GetCellsToRecalculate(ISet<String> names)
+    {
+        LinkedList<String> changed = new LinkedList<String>();
+        HashSet<String> visited = new HashSet<String>();
+        foreach (String name in names)
+        {
+            if (!visited.Contains(name))
+            {
+                Visit(name, name, visited, changed);
+            }
+        }
+        return changed;
+    }
+
+
+    /// <summary>
+    /// A convenience method for invoking the other version of GetCellsToRecalculate
+    /// with a singleton set of names.  See the other version for details.
+    /// </summary>
+    protected IEnumerable<String> GetCellsToRecalculate(String name)
+    {
+        return GetCellsToRecalculate(new HashSet<String>() { name });
+    }
+
+
+    /// <summary>
+    /// A helper for the GetCellsToRecalculate method.
+    /// 
+    ///   -- You should fully comment what is going on below --
+    /// </summary>
+    private void Visit(String start, String name, ISet<String> visited, LinkedList<String> changed)
+    {
+        visited.Add(name);
+        foreach (String n in GetDirectDependents(name))
+        {
+            if (n.Equals(start))
+            {
+                throw new CircularException();
+            }
+            else if (!visited.Contains(n))
+            {
+                Visit(start, n, visited, changed);
+            }
+        }
+        changed.AddFirst(name);
+    }
+
 }
+    
+
+
